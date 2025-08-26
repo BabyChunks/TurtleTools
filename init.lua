b3a6a5b9135460053d0b0e430ec62b54de1a1233
BabyChunks <mondey000@hotmail.com>
2025-08-25 22:04:00 -0400

    check equipped items

--- init.lua
+++ init.lua
@@ -50,6 +50,22 @@ print(string.rep("#", termWidth))
 
 term.redirect(console)
 
+local tools = {{"minecraft:diamond_pickaxe"},{"computercraft:wireless_modem_normal","computercraft:wireless_modem_advanced"}}
+local incomplete = true
+
+while incomplete do
+    local equipment = {}
+    table.insert(equipment, turtle.getEquippedRight().name)
+    table.insert(equipment, turtle.getEquippedLeft().name)
+    incomplete = false
+    for _, tool in ipairs(tools) do
+        if not Lt.tablesOverlap(tool, equipment) then
+            print("Turtle requires a diamond pickaxe and a wireless modem equipped to function.")
+            incomplete = false
+        end
+    end
+end
+
 local function navMenu(options, actions)
     local selected = 1
 
