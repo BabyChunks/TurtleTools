af5dc8092f782b51b83a9290cbe58f4693e1814c
BabyChunks <mondey000@hotmail.com>
2025-08-27 12:16:03 -0400

    made whispering more evident in code

--- init.lua
+++ init.lua
@@ -9,9 +9,9 @@ local files = {
 local gitPath = "https://raw.githubusercontent.com/BabyChunks/TurtleTools/refs/heads/main/"
 local filePath = "/ChunksWare/"
 
-local function initFiles()
-    -- whipser On
-    local whisper = term.redirect(window.create(term.current(), 1, 1, 1, 1, false))
+
+-- whipser On
+local whisper = term.redirect(window.create(term.current(), 1, 1, 1, 1, false))
 
     for _, file in pairs(files) do
         results = fs.find(filePath..file)
@@ -25,11 +25,9 @@ local function initFiles()
         shell.execute("wget", gitPath..file, filePath..file)
     end
 
-    --whisper Off
-    whisper = term.redirect(whisper)
-end
+--whisper Off
+whisper = term.redirect(whisper)
 
-initFiles()
 Lt = require(filePath.."luatools")
 Tt = require(filePath.."quarry")
 St = require(filePath.."settings")
--- setup.lua
+++ setup.lua
@@ -1,19 +1,11 @@
 local results = {}
-local files = {
-    "init.lua",
-    "settings.lua",
-    "luatools.lua",
-    "quarry.lua",
-    "GPS.lua",
-    "GUItools.lua"
-}
+local file = "init.lua"
 local gitPath = "https://raw.githubusercontent.com/BabyChunks/TurtleTools/refs/heads/main/"
 local filePath = "/ChunksWare/"
 
 -- whipser On
 local whisper = term.redirect(window.create(term.current(), 1, 1, 1, 1, false))
 
-for _, file in pairs(files) do
     results = fs.find(filePath..file)
     if #results ~= 0 then
         for _, result in pairs(results) do
@@ -21,7 +13,6 @@ for _, file in pairs(files) do
         end
     end
     shell.execute("wget", gitPath..file, filePath..file)
-end
 
 --whisper Off
 whisper = term.redirect(whisper)
