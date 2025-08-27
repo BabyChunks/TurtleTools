f3193efd5fb8dafb4159d82914d072611f02588d
BabyChunks <mondey000@hotmail.com>
2025-08-27 13:05:07 -0400

    rgb values are between 0 and 1

--- init.lua
+++ init.lua
@@ -44,7 +44,7 @@ local filler2 = ("#"):rep(termWidth)
 
 for fade = 1, 10 do
     corpBanner.clear()
-    local i = 17 + fade * (240 - 17) / 10
+    local i = fade * 0.1
     local txtColour = colours.toBlit(colours.packRGB(i, i, i))
     Gt.drawText(filler2, corpBanner, 1, 1, nil, true, txtColour)
     Gt.drawText(filler1..logo..filler1, corpBanner, nil, nil, "left", true, txtColour)
