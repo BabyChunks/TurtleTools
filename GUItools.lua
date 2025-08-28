68c19f00802da8b6e5639f9a55ea30b936acba7a
BabyChunks <66574238+BabyChunks@users.noreply.github.com>
2025-08-28 10:58:07 -0400

    fdsafdadfa

--- GUItools.lua
+++ GUItools.lua
@@ -1,6 +1,7 @@
 local function drawText(text, monitor, x, y, align, nL, txtColour, bkgColour)
     monitor = monitor or term
-    x, y = x, y or monitor.getCursorPos()
+    x = x or monitor.getCursorPos()
+    _, y = _, y or monitor.getCursorpos()
     print(x, y) _ = io.read()
     txtColour = txtColour or colours.white
     bkgColour = bkgColour or colours.black
