c7249b59887da3fb30ff7f52849c1178b37fc3d4
BabyChunks <66574238+BabyChunks@users.noreply.github.com>
2025-08-30 09:45:08 -0400

    added luatools to setup so we can access args more easily

--- setup.lua
+++ setup.lua
@@ -1,11 +1,14 @@
 local results = {}
-local file = "init.lua"
+local files = {
+    "init.lua",
+    "luatools.lua",
+}
 local gitPath = "https://raw.githubusercontent.com/BabyChunks/TurtleTools/refs/heads/main/"
 local filePath = "/ChunksWare/"
 
 -- whipser On
 local whisper = term.redirect(window.create(term.current(), 1, 1, 1, 1, false))
-
+for _, file in pairs(files) do
     results = fs.find(filePath..file)
     if #results ~= 0 then
         for _, result in pairs(results) do
@@ -13,7 +16,7 @@ local whisper = term.redirect(window.create(term.current(), 1, 1, 1, 1, false))
         end
     end
     shell.execute("wget", gitPath..file, filePath..file)
-
+end
 --whisper Off
 whisper = term.redirect(whisper)
 
