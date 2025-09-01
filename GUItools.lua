a39d9efe74f2b989ca92421630bb9f17faaaa3f8
BabyChunks <66574238+BabyChunks@users.noreply.github.com>
2025-09-01 14:55:58 -0400

    drawText() takes two fewer args

--- GUItools.lua
+++ GUItools.lua
@@ -1,11 +1,10 @@
-local function drawText(text, monitor, x, y, align, nL, txtColour, bkgColour)
+local function drawText(text, monitor, pos, nL, txtColour, bkgColour)
     monitor = monitor or term
-    x = x or monitor.getCursorPos()
-    if not y then _, y = monitor.getCursorPos() end
     txtColour = txtColour or colours.white
     bkgColour = bkgColour or colours.black
 
     local w,h = monitor.getSize()
+    local x, y = monitor.getCursorPos()
 
     if type(txtColour) == "number" then
         txtColour = tostring(colours.toBlit(txtColour)):rep(#text)
@@ -13,17 +12,23 @@ local function drawText(text, monitor, x, y, align, nL, txtColour, bkgColour)
     if type(bkgColour) == "number" then
         bkgColour = tostring(colours.toBlit(bkgColour)):rep(#text)
     end
-
-    if align == "left" then
-        monitor.setCursorPos(1, y)
-        monitor.clearLine()
-    elseif align == "center" then
-        monitor.setCursorPos(w / 2 - #text / 2, y)
-        monitor.clearLine()
-    elseif align == "right" then
-        monitor.setCursorPos(w - #text)
-    elseif align == "centerscreen" then
-        monitor.setCursorPos(w / 2 - #text / 2, h / 2)
+    if type(pos) == "string" then
+        if pos == "left" then
+            monitor.setCursorPos(1, y)
+            monitor.clearLine()
+        elseif pos == "center" then
+            monitor.setCursorPos(w / 2 - #text / 2, y)
+            monitor.clearLine()
+        elseif pos == "right" then
+            monitor.setCursorPos(w - #text)
+        elseif pos == "centerscreen" then
+            monitor.setCursorPos(w / 2 - #text / 2, h / 2)
+            monitor.clearLine()
+        else error("pos should take string arguments -> {left|right|center|centerscreen}")
+        end
+    elseif type(pos) == "table" then
+        if #pos ~= 2 then error("pos should take two arguments-> {x, y}") end
+        monitor.setCursorPos(pos[1], pos[2])
         monitor.clearLine()
     else
         monitor.setCursorPos(x, y)
